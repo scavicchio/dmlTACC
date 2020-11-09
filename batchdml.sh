5097aaffdb94fbcd99fe76cf22fc66704eb2a71f
scavicchio <salvy@nyu.edu>
2020-11-09 11:03:57 -0600

    5

--- batchdml.sh
+++ batchdml.sh
@@ -7,10 +7,10 @@
 #SBATCH -e dml_normal_job.o%j    # Name of stderr output file
 #SBATCH -p vis         # Queue name
 #SBATCH -N 1              # Total number of nodes requested
-#SBATCH -n 24             # Total number of mpi tasks requested
+#SBATCH -n 20             # Total number of mpi tasks requested
 #SBATCH -t 00:15:00       # Run time (hh:mm:ss) - 1.5 hours
 # The next line is required if the user has more than one project
-#SBATCH -A A-yourproject  # Project/allocation number
+#SBATCH -A 819373  # Project/allocation number
 
 # This example will run on 1 node with 24 OpenMP threads
 
